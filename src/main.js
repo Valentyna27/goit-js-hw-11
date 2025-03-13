6e49fb6a2f254e42e6b76da2594af22908598e96
Valentyna <valia.melnik@gmail.com>
2025-03-13 21:10:31 +0100

    fixed files

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { getImages } from './js/pixabay-api';
-import { refillGallery } from './js/render-functions';
+import { refillGallery, clearGallery } from './js/render-functions';
 
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
@@ -34,38 +34,37 @@ form.addEventListener('submit', event => {
   }
 
   loader.style.display = 'block';
-
-  // Clear the gallery before a new request
-  gallery.innerHTML = '';
+  clearGallery();
 
   getImages(inputValue)
     .then(images => {
       loader.style.display = 'none';
+
       if (images.length === 0) {
         iziToast.error({
           title: 'No images',
           titleColor: '#FFFFFF',
           message:
             'Sorry, there are no images matching your search query. Please try again!',
+          backgroundColor: '#EF4040',
+          position: 'bottomRight',
           icon: 'material-icons',
           iconText: 'error',
           iconColor: '#FFFFFF',
           messageColor: '#FFFFFF',
-          backgroundColor: '#EF4040',
-          position: 'bottomRight',
           close: false,
           layout: 2,
           timeout: 10000,
         });
-      } else {
-        imageRequest(images);
-        refillGallery();
+        return;
       }
+      refillGallery(images);
     })
     .catch(error => {
       loader.style.display = 'none';
       iziToast.error({
         title: 'Error',
+        titleColor: '#FFFFFF',
         message: 'Something went wrong!',
         backgroundColor: '#EF4040',
         position: 'bottomRight',
@@ -82,38 +81,3 @@ form.addEventListener('submit', event => {
       form.reset();
     });
 });
-
-function imageRequest(images) {
-  const markup = images
-    .map(
-      image => `<li class="gallery-item">
-        <a class="gallery-link" href="${image.largeImageURL}">
-          <img
-            class="gallery-image"
-            src="${image.webformatURL}"
-            alt="${image.tags}"
-          />
-        </a>
-        <div class="container-image-info">
-        <div class="sub-container">
-        <p class="info-caption">Likes</p>
-          <p class="info-numbers">${image.likes}</p>
-          </div>
-          <div class="sub-container">
-          <p class="info-caption">Views  </p>
-          <p class="info-numbers">${image.views}</p>
-          </div>
-           <div class="sub-container"> 
-           <p class="info-caption">Comments   </p>
-           <p class="info-numbers">${image.comments}</p>
-           </div>
-           <div class="sub-container">
-           <p class="info-caption">Downloads</p> 
-           <p class="info-numbers">${image.downloads}</p>
-           </div>
-        </div>
-      </li>`
-    )
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
